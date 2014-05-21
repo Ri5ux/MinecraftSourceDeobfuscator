e65cf514fea2656254f7713ad71c9d9721740327
Dustin Christensen <dustinchristensen@outlook.com>
2014-05-21 18:47:14 -0500

    Re-formatted

--- src/com/arisux/srgsrc/Application.java
+++ src/com/arisux/srgsrc/Application.java
@@ -4,18 +4,17 @@ import javax.swing.JFrame;
 
 public class Application
 {
-	private static JFrame mainInterface;
-	
-	@SuppressWarnings("unused")
-	public static void main(String[] args)
-	{
-		System.out.println("Application Started");
-		
-		mainInterface = new FrameMain();
-	}
-	
-	public static JFrame getMainInterface()
-	{
-		return mainInterface;
-	}
+    private static JFrame mainInterface;
+
+    public static void main(String[] args)
+    {
+	System.out.println("Application Started");
+
+	mainInterface = new FrameMain();
+    }
+
+    public static JFrame getMainInterface()
+    {
+	return mainInterface;
+    }
 }
